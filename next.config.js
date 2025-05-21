b466467aadb2b0818763ff294f94e2beb3379bf1
donald-debog <donald.sexton@debogautomations.com>
2025-05-21 15:02:53 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['zbmtssocbmkbfagczkmm.supabase.co'], // Update with your Supabase project URL
+    domains: ['https://pub-d97291fa59534fa6b46e358ac977e27c.r2.dev'], // Update with your Supabase project URL
   },
 }
 
